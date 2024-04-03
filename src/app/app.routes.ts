17b792cf07794dd0eef2213146fe419114dce6da
DeanHdz <dean2103@gmail.com>
2024-04-02 21:44:57 -0600

    some pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { LoginPage } from './pages/login/login.page';
+import { RegisterPage } from './pages/register/register.page';
+import { ErrorPage } from './pages/error/error.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "home", component: HomePage},
+    {path: "login", component: LoginPage},
+    {path: "register", component: RegisterPage},
+    {path: "error", component: ErrorPage},
+    {path: "**", redirectTo: "error", pathMatch: "full"},
+];
