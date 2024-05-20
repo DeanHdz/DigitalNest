3e23bd713eeeda1c755a46fc1e7eef01e59f2a25
DeanHdz <dean2103@gmail.com>
2024-05-19 23:24:30 -0600

    Error en redireccionamiento

--- website/src/app/pages/login/login.page.ts
+++ website/src/app/pages/login/login.page.ts
@@ -1,12 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.page.html',
   styleUrl: './login.page.css'
 })
 export class LoginPage {
 
+  constructor(private http: HttpClient) { }
+
+  emailInput: string = "";
+  passInput: string = "";
+
+  public onLogin(): void {
+    this.http.post("http://localhost:8080/api/auth/login", {
+      "email": this.emailInput,
+      "password": this.passInput
+    }).subscribe({ 
+      next: (response : any) => {
+        console.log(response);
+        localStorage.setItem("auth_token", response.token);
+      },
+      error: (error : any) => {
+        console.log(error);
+      }
+    });
+  }
 }
--- website/src/app/pages/register/register.page.ts
+++ website/src/app/pages/register/register.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.page.html',
   styleUrl: './register.page.css'
 })
 export class RegisterPage {
+
+  @ViewChild('registerModal', { static: true }) registerModal!: ElementRef;
+
+  constructor(private http: HttpClient) { }
+
+  modalMessage: string = "";
+
+  nameInput: string = "";
+  emailInput: string = "";
+  passInput: string = "";
+  passInputVerify: string = "";
+
+  public onRegister(): void {
+
+    //Si contraseñas no coinciden, muestra un modal
+    if (this.passInput != this.passInputVerify) {
+      this.modalMessage = "Las contraseñas no coinciden";
+      const modalElement = this.registerModal.nativeElement;
+      const modalInstance = new Modal(modalElement);
+      modalInstance.show();
+      return;
+    }
+
+    this.http.post("http://localhost:8080/api/auth/register", {
+      "username": this.nameInput,
+      "email": this.emailInput,
+      "password": this.passInput
+    }).subscribe({ 
+      next: (response : any) => {
+        console.log(response);
+      },
+      error: (error : any) => {
+        console.log(error);
+      }
+    });
+  }
 
 }
